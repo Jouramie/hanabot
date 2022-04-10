bfb31f887bdac8a40017e57c3cb5417ed95a3516
jouramie <jouramie@hotmail.com>
2022-04-09 20:41:32 -0400

    understand cards

--- read-text.py
+++ read-text.py
@@ -1,36 +1,41 @@
 # Import required packages
 import cv2
+import numpy as np
 import pytesseract
+
+# https://www.geeksforgeeks.org/text-detection-and-extraction-using-opencv-and-ocr/
+# https://www.topcoder.com/thrive/articles/python-for-character-recognition-tesseract
 
 # Mention the installed location of Tesseract-OCR in your system
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 # Read image from which text needs to be extracted
-img = cv2.imread("test/ressources/first-turn-3-players.png")
+image = cv2.imread("target/blue-0.png")
 
 
 # Convert the image to gray scale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-nonoise = cv2.fastNlMeansDenoising(gray, h=10, templateWindowSize=7, searchWindowSize=21)
-cv2.imwrite("nonoise.png", nonoise)
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+filtered = cv2.fastNlMeansDenoising(hsv, h=10, templateWindowSize=7, searchWindowSize=21)
+cv2.imwrite("target/imread/1-filtered.png", filtered)
 
-displayed = img.copy()
+lower, upper = np.array([20, 200, 0]), np.array([200, 255, 255])
 
-# Performing OTSU threshold
-ret, thresh1 = cv2.threshold(nonoise, 200, 255, cv2.THRESH_BINARY_INV)
-# thresh1 = cv2.adaptiveThreshold(nonoise, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-cv2.imwrite("thresh.png", thresh1)
-# text = pytesseract.image_to_string(nonoise)
-boxes = pytesseract.image_to_boxes(nonoise)
+# ret, thresh1 = cv2.threshold(filtered, 0, 255, cv2.THRESH_BINARY_INV)
 
-hImg, wImg = nonoise.shape
+mask = cv2.inRange(filtered, lower, upper)
+cv2.imwrite("target/imread/2-mask.png", mask)
+cv2.imwrite("target/imread/2-1-inv.png", cv2.bitwise_not(mask))
+boxes = pytesseract.image_to_boxes(cv2.bitwise_not(mask))
+print(pytesseract.image_to_string(cv2.bitwise_not(mask)))
+
+
+hImg, wImg, _ = image.shape
 for b in boxes.splitlines():
     b = b.split(" ")
     print(b)
 
     x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (50, 50, 255), 1)
-    cv2.putText(img, b[0], (x, hImg - y + 13), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (50, 205, 50), 1)
+    cv2.rectangle(image, (x, hImg - y), (w, hImg - h), (50, 50, 255), 1)
+    cv2.putText(image, b[0], (x, hImg - y + 13), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (50, 205, 50), 1)
 
-cv2.imwrite("Detected text.png", img)
-cv2.waitKey(0)
+cv2.imwrite("target/imread/3-detected-text.png", image)
--- square-detect.py
+++ square-detect.py
@@ -1,38 +1,54 @@
+from dataclasses import dataclass
+
 import cv2
 import numpy as np
 
 # Load image, grayscale, median blur, sharpen image
-image = cv2.imread("test/resources/first-turn-3-players.png")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-denoise = cv2.fastNlMeansDenoising(gray, h=10, templateWindowSize=7, searchWindowSize=21)
-blur = cv2.medianBlur(denoise, 5)
-sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
+image = cv2.imread("target/red-3.png")
 
-# Threshold and morph close
-# thresh = cv2.threshold(sharpen, 160, 255, cv2.THRESH_BINARY_INV)[1]
-thresh = cv2.adaptiveThreshold(sharpen, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+border = 4
+upper_crop = 24
+cropped = image[border + upper_crop : -border - upper_crop, border:-border]
 
-# Find contours and filter using threshold area
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Convert the image to gray scale
+hsv = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
+filtered = cv2.fastNlMeansDenoising(hsv, h=10, templateWindowSize=7, searchWindowSize=21)
+cv2.imwrite("target/imread/1-filtered.png", filtered)
+
+
+@dataclass(frozen=True)
+class ColorBoundary:
+    color: str
+    lower_bound: np.array
+    upper_bound: np.array
+
+
+red = ColorBoundary("red", np.array([0, 200, 100]), np.array([20, 255, 255]))
+blue = ColorBoundary("blue", np.array([20, 200, 0]), np.array([200, 255, 255]))
+purple = ColorBoundary("purple", np.array([120, 200, 100]), np.array([140, 255, 255]))
+yellow = ColorBoundary("yellow", np.array([0, 0, 0]), np.array([255, 150, 255]))
+green = ColorBoundary("green", np.array([50, 200, 0]), np.array([60, 255, 220]))
+
+boundary = red
+
+mask = cv2.inRange(filtered, boundary.lower_bound, boundary.upper_bound)
+cv2.imwrite("target/imread/2-mask.png", mask)
+inv = cv2.bitwise_not(mask)
+cv2.imwrite("target/imread/2-1-inv.png", inv)
+
+cnts = cv2.findContours(inv, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
 min_area = 100
-max_area = 1500
+max_area = 1000
 image_number = 0
 for c in cnts:
     area = cv2.contourArea(c)
-    if True or area > min_area and area < max_area:
+    if min_area < area < max_area:
         x, y, w, h = cv2.boundingRect(c)
-        ROI = image[y : y + h, x : x + w]
-        cv2.imwrite("target/result/ROI_{}.png".format(image_number), ROI)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 2)
+        card = image[y : y + h, x : x + w]
+        cv2.rectangle(image, (x + border, y + border + upper_crop), (x + w + border, y + h + border + upper_crop), (0, 0, 0), 2)
         image_number += 1
 
-cv2.imwrite("target/1-sharpen.png", sharpen)
-cv2.imwrite("target/2-thresh.png", thresh)
-cv2.imwrite("target/3-close.png", close)
-cv2.imwrite("target/4-image.png", image)
-cv2.waitKey()
+cv2.imwrite("target/imread/3-contour.png", image)
+print(f"This card is {boundary.color} {image_number}")
