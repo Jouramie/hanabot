be5aacff7c6d64b74f8a470e37d2052fac86905a
Alex Gilbert <alexgilbert@yahoo.com>
2022-05-01 16:27:34 -0400

    Bot can now correctly identify anxiety and stall position, but explodes when it happens

--- src/bots/machinabi/machinabi.py
+++ src/bots/machinabi/machinabi.py
@@ -1,7 +1,6 @@
 import logging
 
-from bots.domain.decision import DecisionMaking, PlayDecision, DiscardDecision, Decision, SuitClueDecision
-from bots.domain.model.action import ClueAction, PlayAction
+from bots.domain.decision import DecisionMaking, DiscardDecision, Decision
 from bots.domain.model.game_state import RelativeGameState, GameHistory
 from bots.domain.model.hand import Hand
 
@@ -17,15 +16,30 @@ class Machinabi(DecisionMaking):
             return self.discard_chop(current_game_state)
         pass
 
-    def discard_chop(self, current_game_state: RelativeGameState) -> DiscardDecision:
-        return DiscardDecision(self.get_my_chop_slot(current_game_state))
+    def discard_chop(self, current_game_state: RelativeGameState) -> Decision:
+        chop_slot = self.get_my_chop_slot(current_game_state)
+        if -1 < chop_slot < len(current_game_state.my_hand):
+            return DiscardDecision(chop_slot)
+
+        return self.locked_hand_decision(current_game_state)
+
+    def locked_hand_decision(self, current_game_state: RelativeGameState):
+        if current_game_state.clue_count > 0:
+            return self.stall_clue(current_game_state)
+        return self.anxiety_play(current_game_state)
 
     def get_my_chop_slot(self, current_game_state: RelativeGameState) -> int:
         return self.get_player_chop_slot(current_game_state.my_hand)
 
     def get_player_chop_slot(self, player_hand: Hand) -> int:
-        for slot in range(len(player_hand) - 1, 0, -1):
+        for slot in range(len(player_hand) - 1, -1, -1):
             if not player_hand[slot].is_clued:
                 return slot
 
         return -1
+
+    def stall_clue(self, current_game_state: RelativeGameState):
+        pass
+
+    def anxiety_play(self, current_game_state: RelativeGameState):
+        pass
--- test/bots/machinabi/first_principles/chop_test.py
+++ test/bots/machinabi/first_principles/chop_test.py
@@ -22,7 +22,7 @@ def test_nothing_to_do_should_discard_chop(number_players):
 
 
 @pytest.mark.parametrize("number_players", [number_players for number_players in range(2, 7)])
-def test_nothing_to_do_with_clued_cards_should_discard_chop(number_players):
+def test_nothing_to_do_with_last_card_clued_should_discard_second_last_as_chop(number_players):
     gamestate = get_game_state_nothing_to_do(number_players, Variant.NO_VARIANT)
     gamestate.status.clues = 4
     last_card = gamestate.current_player.hand[-1]
@@ -34,3 +34,34 @@ def test_nothing_to_do_with_clued_cards_should_discard_chop(number_players):
 
     assert isinstance(chosen_decision, DiscardDecision)
     assert chosen_decision.slot == len(gamestate.players[0].hand) - 2
+
+
+@pytest.mark.parametrize("number_players", [number_players for number_players in range(2, 7)])
+def test_nothing_to_do_with_two_last_card_clued_should_discard_third_last_as_chop(number_players):
+    gamestate = get_game_state_nothing_to_do(number_players, Variant.NO_VARIANT)
+    gamestate.status.clues = 4
+    last_card = gamestate.current_player.hand[-1]
+    last_card.receive_clue(ColorClue(last_card.real_card.suit, gamestate.current_player.name, "", 1))
+    second_last_card = gamestate.current_player.hand[-2]
+    second_last_card.receive_clue(ColorClue(second_last_card.real_card.suit, gamestate.current_player.name, "", 2))
+    relative_gamestate, history = assemble_relative_gamestate_and_history(gamestate)
+
+    machinabi = Machinabi()
+    chosen_decision = machinabi.play_turn(relative_gamestate, history)
+
+    assert isinstance(chosen_decision, DiscardDecision)
+    assert chosen_decision.slot == len(gamestate.players[0].hand) - 3
+
+
+@pytest.mark.parametrize("number_players", [number_players for number_players in range(2, 7)])
+def test_nothing_to_do_with_locked_hand_should_not_discard(number_players):
+    gamestate = get_game_state_nothing_to_do(number_players, Variant.NO_VARIANT)
+    gamestate.status.clues = 4
+    for card in gamestate.current_player.hand:
+        card.receive_clue(ColorClue(card.real_card.suit, gamestate.current_player.name, "", 1))
+    relative_gamestate, history = assemble_relative_gamestate_and_history(gamestate)
+
+    machinabi = Machinabi()
+    chosen_decision = machinabi.play_turn(relative_gamestate, history)
+
+    assert not isinstance(chosen_decision, DiscardDecision)
